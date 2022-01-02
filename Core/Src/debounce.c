adf2074b9d39c36d1f77ef6e092dacf4a9d87469
Punit Sai Arani <punitsai36@gmail.com>
2022-01-01 23:57:10 -0700

    Initial commit

---
+++ Core/Src/debounce.c
@@ -0,0 +1,8 @@
+/*
+ * debounce.c
+ *
+ *  Created on: Jan 1, 2022
+ *      Author: punit
+ */
+
+
---
+++ Core/Src/defs.h
@@ -0,0 +1,13 @@
+/*
+ * defs.h
+ *
+ *  Created on: Jan 1, 2022
+ *      Author: punit
+ */
+
+#ifndef SRC_DEFS_H_
+#define SRC_DEFS_H_
+
+
+
+#endif /* SRC_DEFS_H_ */
